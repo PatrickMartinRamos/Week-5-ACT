cf56fe33848af472aae9fd707d2161b7b8025dda
Patrick Ramos <patrick.ramos@ciit.edu.ph>
2023-03-18 17:29:38 +0800

    -added saved player last position -added load player from the last saved position

--- Assets/Scripts/SaveButton.cs
+++ Assets/Scripts/SaveButton.cs
@@ -7,12 +7,30 @@ public class SaveButton : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        // Save the player's position when F2 is pressed
+        if (Input.GetKeyDown(KeyCode.F2))
+        {
+            PlayerPrefs.SetFloat("PlayerX", transform.position.x);
+            PlayerPrefs.SetFloat("PlayerY", transform.position.y);
+            PlayerPrefs.SetFloat("PlayerZ", transform.position.z);
+            PlayerPrefs.Save();
+            Debug.Log("Player position saved.");
+        }
+
+        // Load the player's position when F3 is pressed
+        if (Input.GetKeyDown(KeyCode.F3))
+        {
+            float x = PlayerPrefs.GetFloat("PlayerX");
+            float y = PlayerPrefs.GetFloat("PlayerY");
+            float z = PlayerPrefs.GetFloat("PlayerZ");
+            transform.position = new Vector3(x, y, z);
+            Debug.Log("Player position loaded.");
+        }
     }
 }
